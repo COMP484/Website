ee90d46da2a7d8155324510abf74f2aecba58d7f
haemin9014 <leehaemin9014@gmail.com>
2019-12-05 13:17:36 -0800

    game

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -4,7 +4,73 @@ export class Game extends Component {
     render() {
         return (
             <div>
-                <p>this is game section</p>
+                <div class ="pacman">
+                    <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+                    alt="new" class ="game_image"/>
+                    
+                    <aside class="text_place">
+                        <p>this is game section</p>
+                    </aside>
+                </div>
+                
+                <div class = "start_button">
+                    <p>start game</p>
+                </div>
+                
+                <div class ="pong">
+                    <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+                    alt="new" class ="game_image"      />        
+                
+                    <aside class="text_place">
+                        <p>this is game section</p>
+                    </aside>
+                </div>
+
+                <div class = "start_button">
+                    <p>start game</p>
+                </div>
+
+                <div class ="snake">
+                    <img  src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+                    alt="new" class ="game_image" />
+
+                    <aside class="text_place">
+                        <p>this is game section</p>
+                    </aside>
+                
+                </div>
+
+                <div class = "start_button">
+                    <p>start game</p>
+                </div>
+
+                <div class ="frogger">
+                    <img  src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+                    alt="new" class ="game_image"/>
+
+                    <aside class="text_place">
+                        <p>this is game section</p>
+                    </aside>
+                </div>
+
+                <div class = "start_button">
+                    <p>start game</p>
+                </div>
+
+                <div class ="space">
+                    <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+                    alt="new" class ="game_image"/>
+
+                    <aside class="text_place">
+                        <p>this is game section</p>
+                    </aside>
+
+                </div>
+
+                <div class = "start_button">
+                    <p>start game</p>
+                </div>
+                
             </div>
         )
     }
